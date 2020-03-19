fba6a7ddd5f74ef75c70aaa37f397edb7e9ea124
tabaeias <tabaeias@student.ethz.ch>
2020-03-19 17:52:43 +0100

    last commit

--- scion-criteria-matching.cc
+++ scion-criteria-matching.cc
@@ -606,14 +606,14 @@ main(int argc, char *argv[]) {
 
     beaconing_period = Time(argv[1]);
     expiration_period = Time(argv[2]).ToInteger(Time::NS);
-    std::string file = "/home/tabaeias/workspace/ns-3-allinone/ns-3-dev/topology/" + std::string(argv[4]) + ".xml";
+    std::string file = "./topology/" + std::string(argv[4]) + ".xml";
 
     std::ifstream fin(file.c_str());
     std::ostringstream sstr;
     sstr << fin.rdbuf();
 
     std::string out_path =
-            "/home/tabaeias/workspace/ns-3-allinone/ns-3-dev/results/criteria-matching_" + std::string(argv[4]) + "_" +
+            "./results/criteria-matching_" + std::string(argv[4]) + "_" +
             std::string(argv[1]) + "_" + std::string(argv[2]) + "_" + std::string(argv[3]) + ".txt";
     std::ofstream out(out_path);
     std::cout.rdbuf(out.rdbuf());
