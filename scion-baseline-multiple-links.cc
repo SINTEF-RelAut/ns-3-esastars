4ce85f0bad2c49cf45c7b59887909d3a3f75d24c
Seyedali <tabaeias@student.ethz.ch>
2020-05-28 11:54:43 +0200

    last commit

--- scion-baseline-multiple-links.cc
+++ scion-baseline-multiple-links.cc
@@ -812,7 +812,7 @@ main(int argc, char *argv[]) {
         std::cout << "From: " << index_to_AS_no.at(the_node->as_number) << std::endl;
 
         for (auto const & [src_as, same_src_as_beacons] : the_node->beacon_store) {
-            std::cout << "\t" << "To: " << index_to_AS_no.at(index_to_AS_no.at(src_as)) << std::endl;
+            std::cout << "\t" << "To: " << index_to_AS_no.at(src_as) << std::endl;
 
             for (auto const & same_len_beacons : *same_src_as_beacons) {
                 for (auto const & the_beacon : *same_len_beacons.second) {
