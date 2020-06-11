aa0f64888ed9dbde7fd62e27be891c4d0e0b8d61
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2020-06-11 17:47:16 +0200

    Realized that the functionality of AdjustBeaconValidity is duplicated in UpdateBeaconStore.. of criteria matching file. Since this implementation was cleaner, I swapped them.

--- beaconing_strategy.cpp
+++ beaconing_strategy.cpp
@@ -5,41 +5,53 @@
 #include "beaconing_strategy.h"
 
 void BeaconingStrategy::AdjustBeaconValidity (beacon* the_beacon, SCION_Node* node) {
-    node->now = ns3::Simulator::Now().ToInteger(ns3::Time::NS);
-    uint16_t src_as = *the_beacon->the_path->at(0);
-    if (the_beacon->is_new) {
-        the_beacon->is_new = false;
+        node->now = Simulator::Now().ToInteger(Time::NS);
 
-        if (the_beacon->next_expiration_time > node->now) {
+        if (node->as_number == 0) {
+            std::cout << "################################## " << node->now << " #########################################" << std::endl;
+        }
 
-            if (!the_beacon->is_valid) {
-                if (node->valid_beacons_count_per_src_as.find(src_as) != node->valid_beacons_count_per_src_as.end()) {
-                    node->valid_beacons_count_per_src_as.at(src_as)++;
-                } else {
-                    node->valid_beacons_count_per_src_as.insert(std::make_pair(src_as, 1));
+
+        for (auto const &the_beacon_pair:node->path_map_to_beacon) {
+            beacon* the_beacon = the_beacon_pair.second;
+            uint16_t src_as = the_beacon->the_path->at(0)[0];
+            if (the_beacon->is_new) {
+                the_beacon->is_new = false;
+
+                if (the_beacon->next_expiration_time > node->now) {
+                    if (!the_beacon->is_valid) { // TODO: Change to if, faster than throwing exceptions
+                        try {
+                            node->valid_beacons_count_per_src_as.at(src_as)++;
+                        } catch (std::out_of_range){
+                            node->valid_beacons_count_per_src_as.insert(std::make_pair(src_as, 1));
+                        }
+                    }
+
+                    the_beacon->is_valid = true;
+                    the_beacon->initiation_time = the_beacon->next_initiation_time;
+                    the_beacon->expiration_time = the_beacon->next_expiration_time;
                 }
             }
 
-            the_beacon->is_valid = true;
-            the_beacon->initiation_time = the_beacon->next_initiation_time;
-            the_beacon->expiration_time = the_beacon->next_expiration_time;
-        }
-    }
-
-    if (the_beacon->expiration_time <= node->now && the_beacon->is_valid) {
-        the_beacon->is_valid = false;
-        node->valid_beacons_count_per_src_as.at(src_as) = node->valid_beacons_count_per_src_as.at(src_as) - 1;
-        node->next_round_valid_beacons_count_per_src_as.at(src_as) = node->next_round_valid_beacons_count_per_src_as.at(src_as) - 1;
-
-        if (node->valid_beacons_count_per_src_as.at(src_as) == 0) {
-            node->valid_beacons_count_per_src_as.erase(src_as);
+            if (the_beacon->expiration_time <= node->now && the_beacon->is_valid) {
+                the_beacon->is_valid = false;
+                node->valid_beacons_count_per_src_as.at(src_as)--;
+                node->next_round_valid_beacons_count_per_src_as.at(src_as)--;
+            }
         }
 
-        if (node->next_round_valid_beacons_count_per_src_as.at(src_as) == 0) {
-            node->next_round_valid_beacons_count_per_src_as.erase(src_as);
-        }
-    }
+        std::cout << node->as_number << "\t" <<node->valid_beacons_count_per_src_as.size() << std::endl; // Print number of source ASes
+
 }
+
+
+//if (node->valid_beacons_count_per_src_as.at(src_as) == 0) {
+//    node->valid_beacons_count_per_src_as.erase(src_as);
+//}
+
+//if (node->next_round_valid_beacons_count_per_src_as.at(src_as) == 0) {
+//    node->next_round_valid_beacons_count_per_src_as.erase(src_as);
+//}
 
 // TODO: Is this needed here? Or will this functionality be in the Core & Leaf AS?
 void BeaconingStrategy::DoBeaconing(SCION_Node* node) { // TODO: Second argument for the "allowed interfaces"?
