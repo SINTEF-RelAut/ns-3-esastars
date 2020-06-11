442d4c389627839d862a5fe753f4cf87200471d0
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2020-06-11 18:21:43 +0200

    Realized UpdateBeacon... could be put in baseclass. Also got rid of AdjustBeaconValidity since this functionality only gets called by UpdateBeacon..

--- beaconing_strategy.cpp
+++ beaconing_strategy.cpp
@@ -4,54 +4,6 @@
 
 #include "beaconing_strategy.h"
 
-void BeaconingStrategy::AdjustBeaconValidity (SCION_Node* node) {
-        node->now = ns3::Simulator::Now().ToInteger(ns3::Time::NS);
-        uint16_t src_as = node->as_number;
-
-        if (src_as == 0) { // TODO: Move one lvl up to avoid race condition
-            std::cout << "################################## " << node->now << " #########################################" << std::endl;
-        }
-
-        for (auto const &the_beacon_pair:node->path_map_to_beacon) {
-            beacon* the_beacon = the_beacon_pair.second;
-            uint16_t src_as = the_beacon->the_path->at(0)[0];
-            if (the_beacon->is_new) {
-                the_beacon->is_new = false;
-
-                if (the_beacon->next_expiration_time > node->now) {
-                    if (!the_beacon->is_valid) { // TODO: Change to if, faster than throwing exceptions
-                        try {
-                            node->valid_beacons_count_per_src_as.at(src_as)++;
-                        } catch (std::out_of_range){
-                            node->valid_beacons_count_per_src_as.insert(std::make_pair(src_as, 1));
-                        }
-                    }
-
-                    the_beacon->is_valid = true;
-                    the_beacon->initiation_time = the_beacon->next_initiation_time;
-                    the_beacon->expiration_time = the_beacon->next_expiration_time;
-                }
-            }
-
-            if (the_beacon->expiration_time <= node->now && the_beacon->is_valid) {
-                the_beacon->is_valid = false;
-                node->valid_beacons_count_per_src_as.at(src_as)--;
-                node->next_round_valid_beacons_count_per_src_as.at(src_as)--;
-            }
-        }
-
-        if (node->valid_beacons_count_per_src_as.at(src_as) == 0) {
-             node->valid_beacons_count_per_src_as.erase(src_as);
-        }
-
-        if (node->next_round_valid_beacons_count_per_src_as.at(src_as) == 0) {
-             node->next_round_valid_beacons_count_per_src_as.erase(src_as);
-         }
-
-        std::cout << node->as_number << "\t" <<node->valid_beacons_count_per_src_as.size() << std::endl; // Print number of source ASes
-
-}
-
 // TODO: Is this needed here? Or will this functionality be in the Core & Leaf AS?
 void BeaconingStrategy::DoBeaconing(SCION_Node* node) { // TODO: Second argument for the "allowed interfaces"?
     node->now = ns3::Simulator::Now().ToInteger(ns3::Time::NS);
@@ -69,4 +21,51 @@ bool BeaconingStrategy::generates_loop(beacon const* the_beacon, uint16_t remote
         }
     }
     return false;
+}
+
+void BeaconingStrategy::UpdateBeaconStoreAndCountersBeforeBeaconing(SCION_Node *node){
+    node->now = ns3::Simulator::Now().ToInteger(ns3::Time::NS);
+    uint16_t src_as = node->as_number;
+
+    if (src_as == 0) { // TODO: Move one lvl up to avoid race condition
+        std::cout << "################################## " << node->now << " #########################################" << std::endl;
+    }
+
+    for (auto const &the_beacon_pair:node->path_map_to_beacon) {
+        beacon* the_beacon = the_beacon_pair.second;
+        uint16_t src_as = the_beacon->the_path->at(0)[0];
+        if (the_beacon->is_new) {
+            the_beacon->is_new = false;
+
+            if (the_beacon->next_expiration_time > node->now) {
+                if (!the_beacon->is_valid) { // TODO: Change to if, faster than throwing exceptions
+                    try {
+                        node->valid_beacons_count_per_src_as.at(src_as)++;
+                    } catch (std::out_of_range){
+                        node->valid_beacons_count_per_src_as.insert(std::make_pair(src_as, 1));
+                    }
+                }
+
+                the_beacon->is_valid = true;
+                the_beacon->initiation_time = the_beacon->next_initiation_time;
+                the_beacon->expiration_time = the_beacon->next_expiration_time;
+            }
+        }
+
+        if (the_beacon->expiration_time <= node->now && the_beacon->is_valid) {
+            the_beacon->is_valid = false;
+            node->valid_beacons_count_per_src_as.at(src_as)--;
+            node->next_round_valid_beacons_count_per_src_as.at(src_as)--;
+        }
+    }
+
+    if (node->valid_beacons_count_per_src_as.at(src_as) == 0) {
+        node->valid_beacons_count_per_src_as.erase(src_as);
+    }
+
+    if (node->next_round_valid_beacons_count_per_src_as.at(src_as) == 0) {
+        node->next_round_valid_beacons_count_per_src_as.erase(src_as);
+    }
+
+    std::cout << node->as_number << "\t" <<node->valid_beacons_count_per_src_as.size() << std::endl; // Print number of source ASes
 }
--- beaconing_strategy.h
+++ beaconing_strategy.h
@@ -15,8 +15,8 @@ public:
     virtual void processImmediateReceive(uint16_t src_as_no, uint16_t ingress_if, beacon* the_beacon, std::unordered_map<uint16_t, std::vector<uint16_t>> valid_interfaces, SCION_Node* node) = 0;
 
 protected:
-    static bool generates_loop(beacon const* the_beacon, uint16_t remote_as_no);
-    static void AdjustBeaconValidity (SCION_Node* node);
     static void DoBeaconing(SCION_Node* node);
+    static bool generates_loop(beacon const* the_beacon, uint16_t remote_as_no);
+    static void UpdateBeaconStoreAndCountersBeforeBeaconing(SCION_Node* node);
 };
 #endif //SCION_BEACONING_SIMMULATOR_BEACONING_STRATEGY_H
