cebd15c90f2a85e10f3484a5390b82fc68b0513a
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2020-06-11 15:31:02 +0200

    Added base structure and InitiateBeacons to baseline files. Used the scoping operator to access ns3 functions instead of 'using namespace ns3'

--- baseline.h
+++ baseline.h
@@ -4,5 +4,17 @@
 
 #ifndef SCION_BEACONING_SIMMULATOR_BASELINE_H
 #define SCION_BEACONING_SIMMULATOR_BASELINE_H
-
+#include "beaconing_strategy.h"
+class Baseline : public BeaconingStrategy{
+public:
+    virtual void InitiateBeacons(std::unordered_map<uint16_t, std::vector<uint16_t>> valid_interfaces, SCION_Node *node);
+    virtual void DisseminateBeacons(std::unordered_map<uint16_t, std::vector<uint16_t>> valid_interfaces, SCION_Node *node);
+    virtual void processImmediateReceive();
+private:
+    void UpdateBeaconStoreAndCountersBeforeBeaconing();
+    void send_beacon_to_all_interfaces_with_same_remote_as(beacon* the_beacon, uint16_t remote_as_no);
+    void GenerateBeaconAndSend(beacon *old_beacon, uint16_t self_egress_if_no, uint16_t remote_as_no,
+            uint16_t remote_ingress_if_no, ns3::Ptr<SCION_Node> remote_as, ld latency, ld bwd,
+            bool immediate, ld latency_for_immediate);
+};
 #endif //SCION_BEACONING_SIMMULATOR_BASELINE_H
