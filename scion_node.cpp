629f8c5d82d1c47efed861b6abcb7488f657bad4
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2020-06-11 11:01:30 +0200

    Added the general functionality in SCION_Node (DoInit, FinalPath,calc_final_div + common attributes).

--- scion_node.cpp
+++ scion_node.cpp
@@ -3,3 +3,93 @@
 //
 
 #include "scion_node.h"
+#include "utils.h"
+
+void SCION_Node::DoInitializations() {
+    intra_as_latencies.resize(GetNDevices());
+    for (uint64_t i = 0; i < GetNDevices(); ++i) {
+        intra_as_latencies.at(i).resize(GetNDevices());
+    }
+
+    for (uint32_t i = 0; i < GetNDevices(); ++i) {
+        for (uint32_t j = i + 1; j < GetNDevices(); ++j) {
+            intra_as_latencies.at(i).at(j) = calculate_great_circle_latency(interfaces_coordinates.at(i).first,
+                                                                            interfaces_coordinates.at(i).second,
+                                                                            interfaces_coordinates.at(j).first,
+                                                                            interfaces_coordinates.at(j).second);
+            intra_as_latencies.at(j).at(i) = intra_as_latencies.at(i).at(j);
+        }
+    }
+
+    AS_max_bwd = 0;
+    for (auto const curr_bwd:inter_as_bwds) {
+        if (curr_bwd > AS_max_bwd) {
+            AS_max_bwd = curr_bwd;
+        }
+    }
+}
+
+std::pair<ld, ld> SCION_Node::calculate_final_diversity_scores(beacon *the_beacon) {
+    ld AS_level_diversity_score = 0;
+    ld link_level_diversity_score = 0;
+    int32_t counter = 0;
+    uint16_t src_as = *the_beacon->the_path->at(0);
+    beacons_with_same_src_as *equal_scr_as_beacons = beacon_store.at(src_as);
+    for (auto const &received_if_beacon_vector_pair : *equal_scr_as_beacons) {
+        for (auto const &curr_beacon : *received_if_beacon_vector_pair.second) {
+            if (curr_beacon != the_beacon) {
+                AS_level_diversity_score += AS_level_jaccard_distance_between_two_paths(the_beacon,
+                                                                                        curr_beacon);
+                link_level_diversity_score += link_level_jaccard_distance_between_two_paths(the_beacon,
+                                                                                            curr_beacon);
+                counter++;
+            }
+        }
+    }
+
+    return (std::make_pair(AS_level_diversity_score / counter, link_level_diversity_score / counter));
+}
+
+void SCION_Node::FinalPathEvaluation(std::map<ld, uint64_t> &satisfaction_stat,
+                         std::map<ld, uint64_t> &AS_level_diversity_stat,
+                         std::map<ld, uint64_t> &link_level_diversity_stat) {
+    for (auto const &the_beacon_pair:path_map_to_beacon) {
+        beacon* the_beacon = the_beacon_pair.second;
+        if (the_beacon->is_valid) {
+            std::pair<ld, ld> diversity_scores = this->calculate_final_diversity_scores(the_beacon);
+            ld AS_level_diversity_score = diversity_scores.first;
+            ld link_level_diversity_score = diversity_scores.second;
+            ld latency_score = 1 - the_beacon->latency_stat / 1000;
+            ld bwd_score = the_beacon->bwd_stat / AS_max_bwd;
+
+            ld score =
+                    (latency_score * latency_coef + bwd_score * bandwidth_coef +
+                     AS_level_diversity_score * AS_level_diversity_coef +
+                     link_level_diversity_score * link_level_diversity_coef) /
+                    (latency_coef + bandwidth_coef + AS_level_diversity_coef + link_level_diversity_coef);
+
+            score = roundl(score * 1000) / 1000;
+            AS_level_diversity_score = roundl(AS_level_diversity_score * 1000) / 1000;
+            link_level_diversity_score = roundl(link_level_diversity_score * 1000) / 1000;
+
+
+            if (satisfaction_stat.find(score) != satisfaction_stat.end()) {
+                satisfaction_stat.at(score)++;
+            } else {
+                satisfaction_stat.insert(std::make_pair(score, 1));
+            }
+
+            if (AS_level_diversity_stat.find(AS_level_diversity_score) != AS_level_diversity_stat.end()) {
+                AS_level_diversity_stat.at(AS_level_diversity_score)++;
+            } else {
+                AS_level_diversity_stat.insert(std::make_pair(AS_level_diversity_score, 1));
+            }
+
+            if (link_level_diversity_stat.find(link_level_diversity_score) != link_level_diversity_stat.end()) {
+                link_level_diversity_stat.at(link_level_diversity_score)++;
+            } else {
+                link_level_diversity_stat.insert(std::make_pair(link_level_diversity_score, 1));
+            }
+        }
+    }
+}
--- scion_node.h
+++ scion_node.h
@@ -4,5 +4,67 @@
 
 #ifndef SCION_BEACONING_SIMMULATOR_SCION_NODE_H
 #define SCION_BEACONING_SIMMULATOR_SCION_NODE_H
+//#include "ns3/core-module.h"
+#include "beacon.h"
+#include "ns3/network-module.h"
+#include <unordered_set>
+//#include "ns3/point-to-point-helper.h"
+//#include "ns3/point-to-point-net-device.h"
+//#include "ns3/point-to-point-channel.h"
 
+// TODO: Think about how to unify these?? Seyedali proposed to change all to set
+typedef std::unordered_set<beacon *> beacons_received_from_same_as;
+typedef std::unordered_map<uint16_t, beacons_received_from_same_as *> beacons_with_same_src_as;
+//typedef std::vector<beacon *> beacons_with_equal_length;
+//typedef std::map<uint16_t, beacons_with_equal_length *> beacons_with_same_src_as;
+
+class SCION_Node : public ns3::Node { // TODO: How about aggregating instead?
+
+public:
+
+    //AS properties
+    uint16_t as_number;
+    int64_t now;
+    ld latency_coef, bandwidth_coef, AS_level_diversity_coef, link_level_diversity_coef;
+    int32_t AS_max_bwd;
+
+    // Interfaces Properties *****************************************************************************************************
+    std::vector<uint16_t> neighbors;
+    std::unordered_map<uint16_t, std::vector<uint16_t> > interfaces_per_neighbor_as;
+
+    std::vector<std::pair<ld, ld> > interfaces_coordinates;
+    std::vector<std::vector<ld> > intra_as_latencies;
+    std::vector<int32_t> inter_as_bwds;
+
+    // beacon store structures ***************************************************************************************************
+    std::unordered_map<uint16_t, beacons_with_same_src_as *> beacon_store;
+    std::unordered_map<uint16_t, std::multimap <ld, beacon* >* > beacons_sorted_by_score;
+    std::unordered_map<std::string, beacon*> path_map_to_beacon;
+    // TODO: Think about how to best unify this?
+    //std::unordered_map<uint16_t, beacons_with_same_src_as *> beacon_store;
+    //std::unordered_map<std::string, beacon *> path_map_to_beacon;
+    // helper structures ********************************************************************************************************
+    std::unordered_map<uint16_t, uint64_t> next_round_valid_beacons_count_per_src_as;
+
+    // statistics ***************************************************************************************************************
+    BeaconingStrategy stragegy; // TODO
+
+    // statistics ***************************************************************************************************************
+    std::unordered_map<uint16_t, uint64_t> valid_beacons_count_per_src_as;
+    std::unordered_map<int64_t, std::vector<uint32_t> > bytes_sent_per_interface_per_period;
+
+    SCION_Node(uint16_t as_number, uint32_t system_id, ld latency_coef, ld bandwidth_coef, ld AS_level_diversity_coef,
+           ld link_level_diversity_coef) : Node(
+            system_id), as_number(as_number), latency_coef(latency_coef), bandwidth_coef(bandwidth_coef),
+                                           AS_level_diversity_coef(AS_level_diversity_coef),
+                                           link_level_diversity_coef(link_level_diversity_coef) {}
+
+    void DoInitializations();
+
+    std::pair<ld, ld> calculate_final_diversity_scores(beacon *the_beacon);
+
+    void FinalPathEvaluation(std::map<ld, uint64_t> &satisfaction_stat,
+                             std::map<ld, uint64_t> &AS_level_diversity_stat,
+                             std::map<ld, uint64_t> &link_level_diversity_stat);
+};
 #endif //SCION_BEACONING_SIMMULATOR_SCION_NODE_H
