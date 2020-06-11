0dd754f7eaa06b2a62ba39a3908cee21a8c5181b
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2020-06-11 11:40:55 +0200

    Added the base class for the BeaconingStrategies. 3 Interface functions that will be implemented by the specializations + AdjustBeaconValidity & DoBeaconing.

--- beaconing_strategy.cpp
+++ beaconing_strategy.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "beaconing_strategy.h"
+
+void BeaconingStrategy::AdjustBeaconValidity (beacon* the_beacon, SCION_Node* node) {
+    node->now = ns3::Simulator::Now().ToInteger(ns3::Time::NS);
+    uint16_t src_as = *the_beacon->the_path->at(0);
+    if (the_beacon->is_new) {
+        the_beacon->is_new = false;
+
+        if (the_beacon->next_expiration_time > node->now) {
+
+            if (!the_beacon->is_valid) {
+                if (node->valid_beacons_count_per_src_as.find(src_as) != node->valid_beacons_count_per_src_as.end()) {
+                    node->valid_beacons_count_per_src_as.at(src_as)++;
+                } else {
+                    node->valid_beacons_count_per_src_as.insert(std::make_pair(src_as, 1));
+                }
+            }
+
+            the_beacon->is_valid = true;
+            the_beacon->initiation_time = the_beacon->next_initiation_time;
+            the_beacon->expiration_time = the_beacon->next_expiration_time;
+        }
+    }
+
+    if (the_beacon->expiration_time <= node->now && the_beacon->is_valid) {
+        the_beacon->is_valid = false;
+        node->valid_beacons_count_per_src_as.at(src_as) = node->valid_beacons_count_per_src_as.at(src_as) - 1;
+        node->next_round_valid_beacons_count_per_src_as.at(src_as) = node->next_round_valid_beacons_count_per_src_as.at(src_as) - 1;
+
+        if (node->valid_beacons_count_per_src_as.at(src_as) == 0) {
+            node->valid_beacons_count_per_src_as.erase(src_as);
+        }
+
+        if (node->next_round_valid_beacons_count_per_src_as.at(src_as) == 0) {
+            node->next_round_valid_beacons_count_per_src_as.erase(src_as);
+        }
+    }
+}
+
+void BeaconingStrategy::DoBeaconing(SCION_Node* node) { // TODO: Second argument for the "allowed interfaces"?
+    node->now = ns3::Simulator::Now().ToInteger(ns3::Time::NS);
+
+    node->bytes_sent_per_interface_per_period.insert(std::make_pair(node->now, std::vector<uint32_t > (node->GetNDevices(), 0)));
+
+    this->DisseminateBeacons();
+    this->InitiateBeacons();
+}
--- beaconing_strategy.h
+++ beaconing_strategy.h
@@ -5,4 +5,18 @@
 #ifndef SCION_BEACONING_SIMMULATOR_BEACONING_STRATEGY_H
 #define SCION_BEACONING_SIMMULATOR_BEACONING_STRATEGY_H
 
+#include "beacon.h"
+#include "scion_node.h"
+
+class BeaconingStrategy{
+public:
+    virtual void InitiateBeacons() = 0;
+    virtual void DisseminateBeacons() = 0;
+    virtual void processImmediateReceive() = 0;
+
+private:
+    void AdjustBeaconValidity (beacon* the_beacon, SCION_Node* node);
+    void DoBeaconing(SCION_Node* node);
+
+};
 #endif //SCION_BEACONING_SIMMULATOR_BEACONING_STRATEGY_H
