dbffef9949aa6b4bbe908f48c7b0e6baababc347
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2020-06-12 11:35:33 +0200

    Added GenerateBeaconAndSend to baseline. Still need to unify the beacon store DS and write the methods on the nodes that actually will be scheduled.

--- beaconing_strategy.cpp
+++ beaconing_strategy.cpp
@@ -72,6 +72,6 @@ void BeaconingStrategy::UpdateBeaconStoreAndCountersBeforeBeaconing(SCION_Node *
         beacon* the_beacon = the_beacon_pair.second;
         AdjustBeaconValidity(the_beacon, node);
     }
-    
+
     std::cout << node->as_number << "\t" <<node->valid_beacons_count_per_src_as.size() << std::endl; // Print number of source ASes
 }
