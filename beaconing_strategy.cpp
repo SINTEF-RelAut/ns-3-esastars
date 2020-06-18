8ebf342fbec3c4fa1ebc11589d06bae53dafb786
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2020-06-18 14:15:23 +0200

    Added the ns3:: scope to DynamicCast.

--- beaconing_strategy.cpp
+++ beaconing_strategy.cpp
@@ -4,20 +4,21 @@
 
 #include <ns3/point-to-point-net-device.h>
 #include "beaconing_strategy.h"
+#include "ns3/ptr.h"
 
 void BeaconingStrategy::InitiateBeacons(std::unordered_map<uint16_t, std::vector<uint16_t>> valid_interfaces, SCION_Node *node){
     for (auto const& [remote_as_no, interfaces]: valid_interfaces){
         for (auto const & self_egress_if_no : interfaces) {
-            ns3::Ptr<ns3::PointToPointNetDevice> self_egress_device = DynamicCast<ns3::PointToPointNetDevice>(
+            ns3::Ptr<ns3::PointToPointNetDevice> self_egress_device = ns3::DynamicCast<ns3::PointToPointNetDevice>(
                     node->GetDevice(self_egress_if_no));
-            ns3::Ptr<ns3::PointToPointChannel> channel = DynamicCast<ns3::PointToPointChannel>(
+            ns3::Ptr<ns3::PointToPointChannel> channel = ns3::DynamicCast<ns3::PointToPointChannel>(
                     self_egress_device->GetChannel());
             uint32_t wire = self_egress_device == channel->GetSource(0) ? 0 : 1;
             ns3::Ptr<ns3::PointToPointNetDevice> remote_device = channel->GetDestination(wire);
 
             uint16_t remote_if_no = (uint16_t) remote_device->GetIfIndex();
 
-            ns3::Ptr<SCION_Node> remote_as = (DynamicCast<SCION_Node>(remote_device->GetNode()));
+            ns3::Ptr<SCION_Node> remote_as = (ns3::DynamicCast<SCION_Node>(remote_device->GetNode()));
 
             GenerateBeaconAndSend(NULL, self_egress_if_no, remote_as_no, remote_if_no, node,remote_as,
                                   0.0, node->inter_as_bwds.at(self_egress_if_no), false, 0.0);
@@ -99,15 +100,15 @@ void BeaconingStrategy::UpdateBeaconStoreAndCountersBeforeBeaconing(SCION_Node *
 }
 
 std::tuple<uint16_t, ns3::Ptr<SCION_Node>> BeaconingStrategy::GetRemoteAsInfo(SCION_Node *node, uint16_t egress_interface_no){
-    ns3::Ptr<ns3::PointToPointNetDevice> self_egress_device = DynamicCast<ns3::PointToPointNetDevice>(node->GetDevice(egress_interface_no));
+    ns3::Ptr<ns3::PointToPointNetDevice> self_egress_device = ns3::DynamicCast<ns3::PointToPointNetDevice>(node->GetDevice(egress_interface_no));
 
-    ns3::Ptr<ns3::PointToPointChannel> channel = DynamicCast<ns3::PointToPointChannel>(self_egress_device->GetChannel());
+    ns3::Ptr<ns3::PointToPointChannel> channel = ns3::DynamicCast<ns3::PointToPointChannel>(self_egress_device->GetChannel());
     uint32_t wire = self_egress_device == channel->GetSource(0) ? 0 : 1;
     ns3::Ptr<ns3::PointToPointNetDevice> remote_device = channel->GetDestination(wire);
 
     uint16_t remote_ingress_if_no = (uint16_t) remote_device->GetIfIndex();
 
-    ns3::Ptr<SCION_Node> remote_as = (DynamicCast<SCION_Node>(remote_device->GetNode()));
+    ns3::Ptr<SCION_Node> remote_as = (ns3::DynamicCast<SCION_Node>(remote_device->GetNode()));
 
     return std::make_tuple(remote_ingress_if_no, remote_as);
 }
