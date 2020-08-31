f6469453b5394c907dbf0aad378674f45858acb6
Seyedali <tabaeias@student.ethz.ch>
2020-08-31 11:08:03 +0200

    update scores by receiving any new beacon

--- scion-criteria-matching.cc
+++ scion-criteria-matching.cc
@@ -958,14 +958,14 @@ main(int argc, char *argv[]) {
 
     beaconing_period = Time(argv[1]);
     expiration_period = Time(argv[2]).ToInteger(Time::NS);
-    std::string file = "./topology/" + std::string(argv[4]) + ".xml";
+    std::string file = "/home/tabaeias/topology/" + std::string(argv[4]) + ".xml";
 
     std::ifstream fin(file.c_str());
     std::ostringstream sstr;
     sstr << fin.rdbuf();
 
     std::string out_path =
-            "./results/criteria-matching_" + std::string(argv[4]) + "_" +
+            "/home/tabaeias/results/criteria-matching_" + std::string(argv[4]) + "_" +
             std::string(argv[1]) + "_" + std::string(argv[2]) + "_" + std::string(argv[3]) + ".txt";
     std::ofstream out(out_path);
     std::cout.rdbuf(out.rdbuf());
