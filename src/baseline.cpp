86a72526ef64984a2b48bf1b059ffdf44c87b766
Seyedali <tabaeias@student.ethz.ch>
2020-10-14 10:00:23 +0200

    debug non-zero latency of 2-hop path

--- src/baseline.cpp
+++ src/baseline.cpp
@@ -65,6 +65,7 @@ Baseline::DisseminateBeacons (SCION_Node::neighbour_relation relation)
 
                 for (auto const &the_beacon : beacons)
                 {
+                    assert(the_beacon->the_path.size() != 1 || the_beacon->latency_stat == (float) 0);
                     if (sent_count >= FIXED_BEACONS_NUMBER_TO_SEND)
                     {
                         break;
@@ -114,6 +115,7 @@ Baseline::DisseminateBeacons (SCION_Node::neighbour_relation relation)
                                                remote_as, latency, bwd, false,
                                                0.0);
                     }
+                    assert(the_beacon->the_path.size() != 1 || the_beacon->latency_stat == (float) 0);
                 }
             }
         }
