2a7b32ddf23a9ea569928a74a638f18a640e1792
Seyedali <tabaeias@student.ethz.ch>
2021-07-21 17:28:41 +0200

    debug

--- src/path_server.cpp
+++ src/path_server.cpp
@@ -59,7 +59,7 @@ namespace ns3 {
         }
 
         if (seg_type == path_segment_type::CORE_SEG && DynamicCast<SCION_Core_AS>(node) == NULL) {
-            NS_LOG_DEBUG("core as received core path segment request from " << node->isd_number << ":" << node->as_number  << ":" << host_addr << " between " << GET_ISDN(src_ia) << ":" << GET_ASN(src_ia) << " and " << GET_ISDN(dst_ia) << ":" << GET_ASN(dst_ia));
+            NS_LOG_DEBUG("non-core as received core path segment request from " << node->isd_number << ":" << node->as_number  << ":" << host_addr << " between " << GET_ISDN(src_ia) << ":" << GET_ASN(src_ia) << " and " << GET_ISDN(dst_ia) << ":" << GET_ASN(dst_ia));
             if (dst_ia == 0) {
                 return; //TODO
             } else {
@@ -68,7 +68,7 @@ namespace ns3 {
         }
 
         if (seg_type == path_segment_type::CORE_SEG && DynamicCast<SCION_Core_AS>(node) != NULL) {
-            NS_LOG_DEBUG("non-core as received core path segment request from " << node->isd_number << ":" << node->as_number  << ":" << host_addr << " between " << GET_ISDN(src_ia) << ":" << GET_ASN(src_ia) << " and " << GET_ISDN(dst_ia) << ":" << GET_ASN(dst_ia));
+            NS_LOG_DEBUG("core as received core path segment request from " << node->isd_number << ":" << node->as_number  << ":" << host_addr << " between " << GET_ISDN(src_ia) << ":" << GET_ASN(src_ia) << " and " << GET_ISDN(dst_ia) << ":" << GET_ASN(dst_ia));
             if (dst_ia == 0) {
                 for (auto const & [registered_dst_ia, paths_to_dst_ia] : registered_core_segments) {
                     NS_LOG_DEBUG(GET_ISDN(registered_dst_ia) << ":" << GET_ASN(registered_dst_ia) << " " <<  GET_ISDN(node->isd_number) << ":" << GET_ASN(node->isd_number));
