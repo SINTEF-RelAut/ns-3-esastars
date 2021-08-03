13d21c39fc59195494d998f3b3aa0ac25570634a
Seyedali <tabaeias@student.ethz.ch>
2021-08-03 17:03:59 +0200

    resolving forward declaration of path server

--- headers/path_server.h
+++ headers/path_server.h
@@ -13,21 +13,21 @@
 
 #include "src/SCION/headers/path_segment.h"
 #include "src/SCION/headers/scion_packet.h"
-#include "src/SCION/headers/scion_capable_node.h"
+#include "scion_capable_node.h"
+
 
 namespace ns3 {
+    class SCION_AS;
     class PathServer : public SCIONCapableNode {
     public:
         PathServer(uint32_t system_id, uint16_t isd_number, uint16_t as_number, host_addr_t local_address,
                    double latitude, double longitude, Ptr<SCION_AS> node) :
                    SCIONCapableNode(system_id, isd_number, as_number, local_address, latitude, longitude, node){}
 
-        void process_received_packet(uint16_t local_if, SCIONPacket* packet) override;
 
         void RegisterCorePathSegment (PathSegment& pathSegment, std::string key);
         void RegisterUpPathSegment (PathSegment& pathSegment, std::string key);
         void RegisterDownPathSegment (PathSegment& pathSegment, std::string key);
-
 
     private:
         registered_path_segs_dataset_t registered_core_segments;
@@ -38,6 +38,8 @@ namespace ns3 {
         cached_path_segs_dataset_t cached_up_segments;
         cached_path_segs_dataset_t cached_down_segments;
 
+
+        void process_received_packet(uint16_t local_if, SCIONPacket* packet) override;
 
         void process_local_host_request_for_path (path_segment_type path_type, ia_t src_ia, ia_t dst_ia, host_addr_t host_addr);
 
