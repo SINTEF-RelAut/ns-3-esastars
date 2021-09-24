f8775a174366f16816bc0abf1e198ab280fd3930
Seyedali <tabaeias@student.ethz.ch>
2021-09-24 11:03:16 +0200

    re-trying to use general RunPrallelEvents

--- headers/global_scheduling.h
+++ headers/global_scheduling.h
@@ -13,11 +13,11 @@ namespace ns3 {
 
     void RunParallelEvents (host_addr_t host_addr);
 
-//    template <typename MEM, typename OBJ>
-//    void RunParallelEvents (host_addr_t host_addr, MEM mem_ptr, OBJ obj);
-//
+    template <typename MEM, typename OBJ>
+    void RunParallelEvents (host_addr_t host_addr, MEM mem_ptr, OBJ obj);
 
-    void ScheduleNextEvent (NodeContainer& nodes);
+
+//    void ScheduleNextEvent (NodeContainer& nodes);
 
     void ExecuteNonPeriodicEvents (ns3::NodeContainer& nodes, ns3::Time advance);
 
