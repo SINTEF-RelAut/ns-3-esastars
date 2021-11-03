d3fbf3277f68e461e1cb09fc905ffb86527feb64
Seyedali <tabaeias@student.ethz.ch>
2021-11-03 09:17:48 +0100

    resolve bug. correct with goff not doff, when doff > globalcutoff

--- src/time_server.cpp
+++ src/time_server.cpp
@@ -406,7 +406,7 @@ namespace ns3 {
         if (std::abs(doff) > std::abs(global_cut_off.GetTimeStep())) {
 //            doff = doff > 0 ? std::abs(global_cut_off.GetTimeStep()) : -std::abs(global_cut_off.GetTimeStep());
 //            corr = goff + doff;
-            corr = doff;
+            corr = goff;
         }
 
         correct_local_time(corr, G * time_sync_period);
