0f394d63336d674d065b1f45a01992071e701213
Seyedali <tabaeias@student.ethz.ch>
2021-11-24 15:50:27 +0100

    asserting that the difference between real time difference and offset are bounded by the drifts of the peers

--- src/time_server.cpp
+++ src/time_server.cpp
@@ -628,6 +628,10 @@ namespace ns3 {
     void TimeServer::compare_offs_with_real_offs() {
         for (uint32_t i = 0; i < nodes.GetN(); ++i) {
             Ptr<SCION_AS> node = dynamic_cast<SCION_AS *>(PeekPointer(nodes.Get(i)));
+            if (node->ia_addr == ia_addr) {
+                continue;
+            }
+
             if (poff.find(node->ia_addr) == poff.end()) {
                 std::cout << "NOT FOUND" << std::endl;
                 continue;
