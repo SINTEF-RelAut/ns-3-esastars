b8262994f7a55e1dee55665210844543652d279b
tabaeias <tabaeias@student.ethz.ch>
2022-03-21 14:13:40 +0100

    slightly more than a third ASes to be malicious

---
+++ headers/read_and_schedule_events.h
@@ -0,0 +1,12 @@
+//
+// Created by Seyedali Tabaeiaghdaei on 21.03.22.
+//
+
+#ifndef NS_3_BEACONING_SIMULATOR_READ_AND_SCHEDULE_EVENTS_H
+#define NS_3_BEACONING_SIMULATOR_READ_AND_SCHEDULE_EVENTS_H
+namespace ns3 {
+class ReadAndScheduleEvents {
+
+};
+}
+#endif //NS_3_BEACONING_SIMULATOR_READ_AND_SCHEDULE_EVENTS_H
