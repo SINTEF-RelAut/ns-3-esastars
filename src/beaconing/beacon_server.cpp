e2ed7b0eb82f25c22a35b694956d3ae294015c59
tabaeias <tabaeias@student.ethz.ch>
2022-04-20 10:55:08 +0200

    Revert "run each all ASes beaconings in parallel"
    This reverts commit 9c741a944faf9c5694e565a632f567973ebf5263.

--- src/beaconing/beacon_server.cpp
+++ src/beaconing/beacon_server.cpp
@@ -106,9 +106,9 @@ namespace ns3 {
 
     void BeaconServer::initiate_beacons(neighbour_relation relation) {
         uint32_t neighbors_cnt = AS->neighbors.size();
-//        omp_set_num_threads(NUM_CORE);
+        omp_set_num_threads(NUM_CORE);
 
-//#pragma omp parallel for
+#pragma omp parallel for
         for (uint32_t i = 0; i < neighbors_cnt; ++i) {
             if (AS->neighbors.at(i).second != relation) {
                 continue;
