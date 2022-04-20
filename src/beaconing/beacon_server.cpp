f13d3ba4fcda32d0d85921c51f185709e272a44d
tabaeias <tabaeias@student.ethz.ch>
2022-04-20 10:55:07 +0200

    Revert "run each all ASes beaconings in parallel"
    This reverts commit 225118cdf7dfa50949aa33d3fd736bc40195e969.

--- src/beaconing/beacon_server.cpp
+++ src/beaconing/beacon_server.cpp
@@ -185,7 +185,7 @@ namespace ns3 {
     }
 
     void BeaconServer::update_state_periodic() {
-//        receive_all_beacons();
+        receive_all_beacons();
         auto const &beacons = path_map_to_beacon;
         for (auto const &the_beacon_pair : beacons) {
             Beacon *the_beacon = the_beacon_pair.second;
@@ -285,30 +285,29 @@ namespace ns3 {
 
     void BeaconServer::ReceiveBeacon(Beacon &received_beacon, uint16_t sender_as, uint16_t remote_if,
                                      uint16_t local_if) {
-//        beacon_buffer.at(local_if).push_back(received_beacon);
+        beacon_buffer.at(local_if).push_back(received_beacon);
 
-        #pragma omp critical
-        uint16_t dst_as = UPPER_16_BITS(received_beacon.the_path.at(0));
-
-        bool to_import;
-        bool path_exists;
-        bool existing_path_valid;
-        Beacon *beacon_to_replace;
-        ld replacement_key;
-
-        std::tie(to_import, path_exists, existing_path_valid, beacon_to_replace, replacement_key) =
-                import_policy(received_beacon, sender_as, remote_if, local_if, now);
-
-        if (!to_import) {
-            return;
-        }
-
-        if (!path_exists && beacon_to_replace != NULL) {
-            delete_beacon(beacon_to_replace, replacement_key, dst_as);
-        }
-
-        insert_beacon(received_beacon, dst_as, sender_as, remote_if, local_if, path_exists, existing_path_valid,
-                      beacon_to_replace);
+//        uint16_t dst_as = UPPER_16_BITS(received_beacon.the_path.at(0));
+//
+//        bool to_import;
+//        bool path_exists;
+//        bool existing_path_valid;
+//        Beacon *beacon_to_replace;
+//        ld replacement_key;
+//
+//        std::tie(to_import, path_exists, existing_path_valid, beacon_to_replace, replacement_key) =
+//                import_policy(received_beacon, sender_as, remote_if, local_if, now);
+//
+//        if (!to_import) {
+//            return;
+//        }
+//
+//        if (!path_exists && beacon_to_replace != NULL) {
+//            delete_beacon(beacon_to_replace, replacement_key, dst_as);
+//        }
+//
+//        insert_beacon(received_beacon, dst_as, sender_as, remote_if, local_if, path_exists, existing_path_valid,
+//                      beacon_to_replace);
     }
 
     void BeaconServer::receive_all_beacons() {
@@ -316,7 +315,7 @@ namespace ns3 {
             for (auto it = beacon_buffer.at(local_if).begin();
                  it != beacon_buffer.at(local_if).end(); ++it) {
 
-                Beacon & received_beacon = *it;
+                Beacon & received_beacon = *beacon_buffer.at(local_if).erase(it--);
                 uint16_t dst_as = UPPER_16_BITS(received_beacon.the_path.at(0));
                 uint16_t sender_as = UPPER_16_BITS(received_beacon.the_path.back());
                 uint16_t remote_if = SECOND_UPPER_16_BITS(received_beacon.the_path.back());
@@ -340,8 +339,6 @@ namespace ns3 {
 
                 insert_beacon(received_beacon, dst_as, sender_as, remote_if, local_if, path_exists, existing_path_valid,
                               beacon_to_replace);
-
-                beacon_buffer.at(local_if).erase(it--);
             }
         }
     }
