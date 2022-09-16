eb9406b932ebdbef6072ce902891c6d31aa66da0
tabaeias <tabaeias@student.ethz.ch>
2022-09-16 20:03:55 +0200

    apply ns-3 coding style, copyright header added

--- src/beaconing/scionlab_algo.cpp
+++ src/beaconing/scionlab_algo.cpp
@@ -1,8 +1,21 @@
-/**
- * @file scionlab_algo.cpp
- * @authors Seyedali Tabaeiaghdaei
- * @date 2021
- * @see scionlab_algo.h
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2022 ETH Zuerich
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Seyedali Tabaeiaghdaei seyedali.tabaeiaghdaei@inf.ethz.ch
  */
 
 #include "src/SCION/headers/beaconing/scionlab_algo.h"
